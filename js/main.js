843b6ec917f80bc8441c3146d9e44457b4fe5ceb
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-12-28 17:48:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,3 +44,17 @@ function handlerSubmit() {
   ).padStart(2, "0")}.${year}`;
   orderDate.textContent = `Order date: ${formattedDate}`;
 }
+
+
+// Burger menu
+document.addEventListener("click", documentClick);
+
+function documentClick(e) {
+  const targetItem = e.target;
+  console.log(targetItem);
+  if (targetItem.closest(".icon-menu")) {
+    document.documentElement.classList.toggle("menu-open");
+  } else if (targetItem.closest(".menu__link")) {
+    document.documentElement.classList.toggle("menu-open");
+  }
+}
